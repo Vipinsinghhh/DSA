bb3832eaceef6a7f493f13efbabde13cd8c45177
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2025-07-06 20:28:10 +0530

    added a basic codes of cpp

---
+++ Basic_CPP/PQ9.cpp
@@ -0,0 +1,16 @@
+//Nested loops
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n=10;
+    for(int i=0;i<=n;i++){
+        int m=10;
+        for(int j=1;j<=m;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
