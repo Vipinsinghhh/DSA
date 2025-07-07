19e9045c4cbfcf26ce556a54708acae78c9126da
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2025-07-07 23:48:01 +0530

    added a basic pattern problem in cpp

---
+++ Patterns/Q1.cpp
@@ -0,0 +1,20 @@
+// basic square pattern printing
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+
+    cout<<"enter the number:";
+    cin>>n;
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
