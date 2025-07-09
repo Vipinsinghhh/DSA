f4bab495a4f42d654b0da91fc92914dbbba99513
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2025-07-10 00:06:30 +0530

    added a basic program of functions

---
+++ Functions/Sumof2num.cpp
@@ -0,0 +1,14 @@
+// program for calculate teh sum of 2 number with the functions
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int sum(int a,int b){
+    int sum=a+b;
+    return sum;
+}
+
+int main(){
+    cout<<sum(10,5);
+    return 0;
+}
