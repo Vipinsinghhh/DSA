2ab5b4c0458cfa65c04b2cd60be712628ba1260e
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2025-07-09 23:59:08 +0530

    added a basic program of functions

---
+++ Functions/Syntax.cpp
@@ -0,0 +1,14 @@
+// basic hello world program for syntax understanding
+
+#include<bits/stdc++.h>
+using namespace std;
+
+//function declaration
+void printHello(){
+    cout<<"hello";
+}
+
+int main(){
+    printHello();
+    return 0;
+}
