3dc8d91f740057e477dbea1852f83fec025e296a
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2025-08-17 19:58:34 +0530

    convert decimal to binary by using functions

---
+++ Binary_number_system/DecimalToBinary2.cpp
@@ -0,0 +1,28 @@
+//convert decimal to binary using function
+
+#include<iostream>
+using namespace std;
+
+int decToBinary(int decNum){
+    int ans = 0;
+    int pow = 1;
+
+    while(decNum>0){
+        int rem = decNum % 2;
+        decNum = decNum / 2;
+
+        ans = ans + ( rem * pow);
+        pow = pow * 10;
+    }
+    return ans;
+}
+
+int main(){
+    int decNum;
+    cout<<"enter decimal number:";
+    cin>>decNum;
+
+    cout<<"Binary num of "<<decNum<<" is: "<<decToBinary(decNum)<<endl;
+
+    return 0;
+}
