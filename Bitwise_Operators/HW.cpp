32245a307100d31e8a5876ea08ecca66881111e9
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2025-08-22 01:32:06 +0530

    added solutions of given HW questions

---
+++ Bitwise_Operators/HW.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout<<"answer of 6&10 is: "<<(6 & 10)<<endl;
+    cout<<"answer of 6|10 is: "<<(6 | 10)<<endl;
+    cout<<"answer of 6^10 is: "<<(6 ^ 10)<<endl;
+
+    cout<<"answer of 10<<2 is: "<<(10 << 2)<<endl;
+    cout<<"answer of 10>>1 is: "<<(10 >> 1)<<endl;
+    
+    return 0;
+}
