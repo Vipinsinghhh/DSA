08738214f12dc0c6cefd7c7f5d36482f167e7d3f
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2025-08-23 01:37:02 +0530

    Find smallest number in array

---
+++ Array/smallestNum.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int size;
+    cout<<"Enter the size of the array: ";
+    cin>>size;
+
+    int arr[size];
+
+    for(int i=0;i<size;i++){
+        cout<<"enter the value of array index: "<<i<<endl;
+        cin>>arr[i];
+    }
+
+    int smallest = INT_MAX;
+
+    for(int i=0;i<size;i++){
+        if(arr[i] < smallest){
+            smallest = arr[i];
+        }
+    }
+    cout<<"smalllest value in array is: "<<smallest;
+
+    return 0;
+}
