2109dbd42146be384b822368fd3b50bc1dd93187
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2025-08-31 02:57:59 +0530

    pattern 2

---
+++ Patterns2/P2.cpp
@@ -0,0 +1,23 @@
+/*
+11111
+2222
+333
+44
+5
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[5] = {1,2,3,4,5};
+    int size =5;
+
+    for(int i=0;i<size;i++){
+        for(int j=i;j<size;j++){
+            cout<<arr[i];
+        }
+        cout<<endl;
+    }
+    return 0;
+}
