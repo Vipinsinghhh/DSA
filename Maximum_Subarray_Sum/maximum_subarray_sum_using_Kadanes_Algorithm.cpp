ebca8759b5dbe8890568883fbf96a73f5a7a4f12
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2025-08-31 21:10:43 +0530

    Find maximum subarray sum using kadane's algorithm

---
+++ Maximum_Subarray_Sum/maximum_subarray_sum_using_Kadanes_Algorithm.cpp
@@ -0,0 +1,26 @@
+
+#include <bits/stdc++.h>
+#include<vector>
+using namespace std;
+
+int MaxSum_SubArray(vector<int>&nums){
+    int currentSum = 0;
+    int maxSum = INT_MIN;
+
+    for(int val: nums){
+        currentSum = currentSum + val;
+        maxSum = max(currentSum, maxSum);
+
+        if(currentSum < 0){
+            currentSum = 0;
+        }
+    }
+    return maxSum;
+}
+int main(){
+    vector<int>nums = { 3,-4,5,4,-1,7,-8};
+
+    cout<<"maximum subarray sum is:"<<MaxSum_SubArray(nums);
+
+    return 0;
+}
