de0e27917e5dcf4c2e920e3a4e7db87e5f46df02
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2025-09-04 04:46:31 +0530

    less optimised solution of find majority element by sorting element of array

--- Majority_Element_Ques/majority_element_LessOptimsed.cpp
+++ Majority_Element_Ques/majority_element_LessOptimsed.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 int majority(int arr[], int size){
+    sort(arr, arr+size);
+
     int count = 1; int ans = arr[0];
 
     for(int i=1;i<size;i++){
@@ -18,7 +20,7 @@ int majority(int arr[], int size){
     return ans;
 }
 int main(){
-    int arr[] = {1,1,1,2,2,2,2,2};
+    int arr[] = {1,2,9,5,4,2,2,2,2};
     int size = sizeof(arr)/ sizeof(arr[0]);
 
     cout<<majority(arr,size);
