816df7fb3f28b5d09b4a3c35c45dd3931fdac738
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2025-09-04 04:19:50 +0530

    Optimes way of Find majority element without sorting array

---
+++ Majority_Element_Ques/majority_element_LessOptimsed.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int majority(int arr[], int size){
+    int count = 1; int ans = arr[0];
+
+    for(int i=1;i<size;i++){
+        if(arr[i] == arr[i-1]){
+            count++;
+        }else{
+            count =1;
+            ans = arr[i];
+        }
+        if(count > size/2){
+            return ans;
+        }
+    }
+    return ans;
+}
+int main(){
+    int arr[] = {1,1,1,2,2,2,2,2};
+    int size = sizeof(arr)/ sizeof(arr[0]);
+
+    cout<<majority(arr,size);
+
+    return 0;
+}
