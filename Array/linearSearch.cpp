cf6ea18b001b84ba43db76ca67ed4b94dc54a067
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2026-01-10 20:21:05 +0530

    linear search

--- Array/linearSearch.cpp
+++ Array/linearSearch.cpp
@@ -1,35 +1,36 @@
 #include<iostream>
 using namespace std;
 
-void update(int arr[], int n) {
+bool search(int arr[], int size, int key) {
 
-    cout << endl << "Inside the function" <<endl;
-    
-    //updating array's first element
-    arr[0] = 120;
+    for( int i = 0; i<size; i++ ) {
 
-    //printing the array
-    for(int i=0; i<3; i++) {
-        cout << arr[i] <<" ";
-    } cout<<endl;
+        if( arr[i] == key) {
+            return 1;
+        }
 
-
-    cout << "Going back to main function" << endl;
+    }
+    return 0;
 }
 
 
 int main() {
 
-    int arr[3] = {1,2,3};
+    int arr[10] = { 5, 7, -2, 10, 22, -2, 0, 5, 22, 1};
 
-    update(arr, 3);
+    cout <<" Enter the element to search for " << endl; 
+    int key;
+    cin >> key;
 
-    //printing the array
-    cout<< endl << "Printing in main function" << endl;
-    for(int i=0; i<3; i++) {
-        cout << arr[i] <<" ";
+    bool found = search(arr, 10, key);
+
+    if( found ) {
+        cout <<" Key is present "<< endl;
     }
-    cout << endl;
+    else{
+        cout <<" Key is absent " << endl;
+    }
+
 
     return 0;
 }
