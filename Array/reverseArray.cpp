f7a3d3a7ab8cd39a0fca6e497c3d979072f15215
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2026-07-25 18:02:47 +0530

    reverse an array by different approaches

--- Array/reverseArray.cpp
+++ Array/reverseArray.cpp
@@ -1,5 +1,7 @@
 //Reverse an array using 2 pointers approach
 
+
+//using inbuilt swap function
 #include<iostream>
 using namespace std;
 
@@ -24,3 +26,65 @@ int main(){
     }
     return 0;
 }
+
+
+//using temp variable
+
+// #include <iostream>
+// using namespace std;
+
+// void reverseArr(int arr[], int size){
+//     int start = 0;
+//     int end = size - 1;
+//     while(start < end){
+//         int temp = arr[start];
+//         arr[start] = arr[end];
+//         arr[end] = temp;
+//     start++; 
+//     end--;
+//     }
+// }
+// int main() {
+    
+//     int arr[5] = {1,2,3,4,5};
+    
+//     reverseArr(arr,5);
+    
+//     cout << "reversed array is: ";
+    
+//     for (int i=0; i<5; i++){
+//         cout << arr[i] << "  ";
+//     }
+
+//     return 0;
+// }
+
+
+//using for loop
+
+// #include <iostream>
+// using namespace std;
+
+// void reverseArr(int arr[], int size){
+    
+//     for(int i=0;i<size/2;i++){
+//         int temp = arr[i];
+//         arr[i] = arr[size-i-1];
+//         arr[size-i-1] = temp;
+//     }
+// }
+
+// int main() {
+
+// int arr[5] = {1,2,3,4,5};
+
+// reverseArr(arr, 5);
+
+// cout << "Reversed array is: ";
+
+// for (int i =0; i< 5; i++){
+//     cout<< arr[i] << "  ";
+// }
+
+//     return 0;
+// }
