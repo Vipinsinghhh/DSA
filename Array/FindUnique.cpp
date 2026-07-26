8396c621553fc0081f0c6cd8a13088be2f085d83
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2026-07-27 00:41:32 +0530

    Find unique element, brute force and optmised approach

--- Array/FindUnique.cpp
+++ Array/FindUnique.cpp
@@ -21,3 +21,36 @@ int main(){
 
     return 0;
 }
+
+
+//less optimised approach
+// #include<iostream>
+// using namespace std;
+
+// int findUnique(int arr[], int size){
+//     for(int i=0;i<size;i++){
+//         int count =0;
+//         for(int j=0;j<size;j++){
+//             if(i != j && arr[i] == arr[j]){
+//                 count ++;
+//             }
+//         }
+//         if(count ==0){
+//             return arr[i];
+//         }
+//     }
+//     return -1;
+// }
+
+// int main(){
+//     int arrA[9] = {1,1,3,3,8,4,4,2,2};
+//     int arrB[5] = {2, 4, 2, 3, 3};
+
+//     cout << "unique number in arrayA is: "<<findUnique(arrA,9);
+
+//     cout << endl;
+
+//     cout << "unique number in arrayB is: "<<findUnique(arrB,5);
+
+//     return 0;
+// }
