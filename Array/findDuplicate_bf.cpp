f204adf3950bb886375aedbae1d1ef864c1f5548
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2026-07-27 22:52:00 +0530

    Ignore executable files

---
+++ Array/findDuplicate_bf.cpp
@@ -0,0 +1,29 @@
+//find duplicate elements in array
+//brute force
+// CodeStudio - [https://bit.ly/3dm6bdZ]
+
+#include<iostream>
+using namespace std;
+
+int findDuplicate(int arr[], int size){
+    for(int i=0; i<size; i++){
+        int count = 0;
+        for(int j=0; j<size; j++){
+            if(i != j && arr[i] == arr[j]){
+                count++;
+            }
+        }
+        if(count > 0){
+            return arr[i];
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int arr[5] = {1,2,3,4,3};
+
+    cout << "Duplicate number in array is: "<< findDuplicate(arr, 5);
+
+    return 0;
+}
